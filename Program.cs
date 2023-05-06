49eef7c1fa9576cf2a4ea5f5c03482f205fa7b8d
GabrielLareglia <laregliagabriel@gmail.com>
2023-05-06 19:19:21 -0300

    Concluido

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("--- Fluxograma Da Engenharia ---");
+Console.WriteLine();
+
+Console.Write("O objeto está se movendo (S/N)?\n");
+bool estaEmMovimento = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "S";
+
+Console.Write("Deveria?\n");
+bool deveria = Console.ReadLine()!.Trim().Substring(0, 1).ToUpper() == "N";
+
+string mensagem;
+
+if (!estaEmMovimento)
+{
+    
+    if (!deveria)
+    {
+        mensagem = "Perfeito!";
+    }
+    else
+    {
+        mensagem = "Use WD-40.";
+    }
+}
+else
+{
+    if (deveria)
+    {
+        mensagem = "Excelente!";
+    }
+    else
+    {
+        mensagem = "Use Fita Adesiva.";
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine(mensagem);
